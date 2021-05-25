2b359ab8274be0d8bfec373a62ae37b5195d2ecd
Sergey Pashov <pashov155@outlook.com>
2021-05-25 22:39:58 +0300

    Название формы

--- Client/View/MainWindow.Designer.cs
+++ Client/View/MainWindow.Designer.cs
@@ -163,7 +163,7 @@ namespace Client.View
             this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "P2P-Messenger ";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
